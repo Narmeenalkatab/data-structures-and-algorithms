f9dbbcc12dc930859ecfb7eb2985c02ca5106fd1
Narmeen alkatab <narmeen.kattab1@gmail.com>
2023-08-22 20:34:13 +0300

    whiteboard is added

---
+++ readmefiles/graphbusiness.md
@@ -0,0 +1,13 @@
+
+# White board
+
+![GRAPH](../images/BUSSINESS.jpg)
+
+# Run in terminal
+
+__pytest tests/test_graph.py__
+
+# Implementation Code and Test
+[Code](../scripts/graph.py)
+
+[Test](../tests/test_graph.py)
--- scripts/graphbusiness.py
+++ scripts/graphbusiness.py
@@ -1,19 +1,74 @@
 class Graph:
+    """
+    A class representing a graph and its operations.
+
+    Attributes:
+        vertices (dict): A dictionary representing the vertices (nodes) and their neighbors.
+
+    Methods:
+        add_vertex(vertex): Adds a vertex to the graph.
+        add_edge(start_vertex, end_vertex, cost): Adds an edge with a cost between two vertices.
+        get_neighbors(vertex): Returns the neighbors of a vertex.
+    """
+
     def __init__(self):
+        """
+        Initializes an empty graph with no vertices.
+        """
         self.vertices = {}
 
     def add_vertex(self, vertex):
+        """
+        Adds a vertex to the graph.
+
+        Args:
+            vertex: The vertex to be added.
+
+        Returns:
+            None
+        """
         if vertex not in self.vertices:
             self.vertices[vertex] = []
 
     def add_edge(self, start_vertex, end_vertex, cost):
+        """
+        Adds an edge with a cost between two vertices.
+
+        Args:
+            start_vertex: The starting vertex of the edge.
+            end_vertex: The ending vertex of the edge.
+            cost: The cost associated with the edge.
+
+        Returns:
+            None
+        """
+
         if start_vertex in self.vertices and end_vertex in self.vertices:
             self.vertices[start_vertex].append((end_vertex, cost))
 
     def get_neighbors(self, vertex):
+        """
+        Returns the neighbors of a vertex.
+
+        Args:
+            vertex: The vertex for which to retrieve neighbors.
+
+        Returns:
+            list: A list of tuples representing neighbors and their associated costs.
+        """
         return self.vertices.get(vertex, [])
 
 def business_trip(graph, city_names):
+    """
+    Calculates the total cost of traveling between cities in the given graph.
+
+    Args:
+        graph (Graph): The graph containing city connections and costs.
+        city_names (list): The list of city names to travel through.
+
+    Returns:
+        int or None: The total cost of the trip, or None if the trip is not possible.
+    """
     if not graph or not city_names:
         return None
 
