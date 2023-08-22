a98a4348572bdfbb50652619e09ec9dbf9b914b6
Narmeen alkatab <narmeen.kattab1@gmail.com>
2023-08-22 19:53:11 +0300

    whiteboard is added

--- readmefiles/graphdepth.md
+++ readmefiles/graphdepth.md
@@ -1,3 +1,30 @@
+
+  ## the approach for the depth-first traversal algorithm step by step:
+
+Initialization:
+
+Initialize an empty set called visited to keep track of nodes that have been visited during the traversal.
+Initialize an empty list called result to store the nodes in the order they are visited.
+Depth-First Search Function (dfs):
+
+Create a recursive function named dfs(node) that takes a node as an argument.
+Inside the dfs function:
+Add the current node to the visited set.
+Append the current node to the result list.
+Check if the current node exists in the graph:
+If it does, iterate through its neighbors.
+For each neighbor:
+If the neighbor has not been visited, recursively call dfs(neighbor).
+Start Traversal:
+
+Call the dfs function with the start_node as the argument to begin the traversal from the specified starting point.
+Return Result:
+
+After the traversal is complete, return the result list containing the order in which nodes were visited.
+
+# whiteboard
+![depth](../images/Gdepth.jpg)
+
 # Run in terminal
 
 __pytest tests/test_graphdepth.py__
--- scripts/graphdepth.py
+++ scripts/graphdepth.py
@@ -1,13 +1,46 @@
 class Graph:
+    """
+    A class representing a graph and its operations.
+
+    Attributes:
+        graph (dict): A dictionary representing the graph's nodes and their neighbors.
+
+    Methods:
+        add_edge(node, neighbor): Adds an edge between a node and its neighbor.
+        depth_first(start_node): Performs a depth-first traversal starting from the given node.
+    """
+
     def __init__(self):
+        """
+        Initializes an empty graph.
+        """
         self.graph = {}
 
     def add_edge(self, node, neighbor):
+        """
+        Adds an edge between a node and its neighbor.
+
+        Args:
+            node: The node to which the neighbor will be added.
+            neighbor: The neighbor node to be added.
+
+        Returns:
+            None
+        """
         if node not in self.graph:
             self.graph[node] = []
         self.graph[node].append(neighbor)
 
     def depth_first(self, start_node):
+        """
+        Performs a depth-first traversal starting from the given node.
+
+        Args:
+            start_node: The starting node for the depth-first traversal.
+
+        Returns:
+            list: A list containing the nodes visited during the depth-first traversal.
+        """
         visited = set()
         result = []
 
